1f2618107cec75122c7a0ab4951c8ebc132602f0
nmendez4 <nmendez6594@gmail.com>
2022-12-08 10:53:52 -0500

    updated portfolio and project tabs

---
+++ src/components/Portfolio.js
@@ -0,0 +1,89 @@
+import React from "react";
+import Project from './Project';
+
+// list of all past/current projects
+// & their description + details
+const projects = [
+{
+    id: 0,
+    title: "",
+    languages: "",
+    packages: "",
+    image: "",
+    description: "",
+    repository: "",
+    live: ""
+},
+{
+    id: 1,
+    title: "",
+    languages: "",
+    packages: "",
+    image: "",
+    description: "",
+    repository: "",
+    live: ""
+},
+{
+    id: 2,
+    title: "",
+    languages: "",
+    packages: "",
+    image: "",
+    description: "",
+    repository: "",
+    live: ""
+},
+{
+    id: 3,
+    title: "",
+    languages: "",
+    packages: "",
+    image: "",
+    description: "",
+    repository: "",
+    live: ""
+},
+{
+    id: 4,
+    title: "",
+    languages: "",
+    packages: "",
+    image: "",
+    description: "",
+    repository: "",
+    live: ""
+},
+{
+    id: 5,
+    title: "",
+    languages: "",
+    packages: "",
+    image: "",
+    description: "",
+    repository: "",
+    live: ""
+},
+{
+    id: 6,
+    title: "",
+    languages: "",
+    packages: "",
+    image: "",
+    description: "",
+    repository: "",
+    live: ""
+}
+];
+
+function Portfolio() {
+    return (
+        <div>
+            <p className="card">
+               My Portfolio
+            </p>
+        </div>
+    );
+}
+
+export default Portfolio;
---
+++ src/components/Project.js
@@ -0,0 +1,65 @@
+import React from "react";
+
+function Project(props) {
+    return (
+      <div>
+        <div className="container">
+          {props.projects.map((project) => (
+            <div className="col">
+              <div className="card">
+                <div className="card-image">
+                  <figure className="image">
+                    <a href={project.live} target="_blank" rel="noreferrer">
+                      <img src={process.env.PUBLIC_URL + project.image} alt="Placeholder project" />
+                    </a>
+                  </figure>
+                </div>
+                <div className="card">
+                  <div className="media">
+                    <div className="media-left"></div>
+                    <div className="media-content">
+                      <p className="title" key={project.id}>
+                        {project.title}
+                      </p>
+                    </div>
+                  </div>
+  
+                  <div className="col">
+                    {project.description}
+                    <br />
+                    <br />
+                    <div className="card">
+                      Languages: {project.languages}
+                      <br />
+                      Packages/Libraries: {project.packages}
+                    </div>
+                    <div className="card">
+                      <footer className="card-footer">
+                        <a
+                          href={project.repository}
+                          className="card"
+                          target="_blank" rel="noreferrer"
+                        >
+                          Repository Link
+                        </a>
+                        <br />
+                        <a
+                          href={project.live}
+                          className="card"
+                          target="_blank" rel="noreferrer"
+                        >
+                          Live Site 
+                        </a>
+                      </footer>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+  
+  export default Project;
