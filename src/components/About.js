ed3b00e8794125cfb261e599b02e891602e6448e
nmendez4 <nmendez6594@gmail.com>
2022-12-08 10:19:11 -0500

    outline and structuring

---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function About() {
+    return (
+        <div>
+            <p className="card">About Me</p>
+            <hr />
+            {/* <img className="my-photo" src={} alt="" /> need to add image address */}
+            <p className="content">
+                {/* introduction text here */}
+            </p>
+            <p className="content">
+                {/* description text here */}
+            </p>
+        </div>
+    );
+}
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import { validateEmail } from "../utils/helpers";
+
+function Contact() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: ''});
+    const [errorMessage, setErrorMessage] = useState('');
+    const { name, email, message } = formState;
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!errorMessage) {
+            console.log('Subtmit Form', formState)
+        }
+    };
+
+    const handleChange = (e) => {
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            if (!isValid) {
+                setErrorMessage('Email is invalid.');
+            } else {
+                setErrorMessage('');
+            }
+        } else {
+            if (!e.target.value.length) {
+                setErrorMessage(`A ${e.target.name} is required.`)
+            } else {
+                setErrorMessage('');
+            }
+        }
+        if (!errorMessage) {
+            setFormState({...formState, [e.target.name]: e.target.value });
+            console.log('Handle Form', formState)
+        }
+    };
+
+    return (
+        <div>
+            <p className="card">Contact Me</p>
+            <p className="card">Please include your full name and a message below, thank you!</p>
+            <hr />
+            <form id="contact-form" onSubmit={handleSubmit}>
+                <div className="field">
+                    <label className="label" htmlFor="name">Name:</label>
+                    <input className="input" type={text} name="name" defaultValue={name} onBlur={handleChange}></input>
+                </div>
+                <div className="field">
+                    <label className="label" htmlFor="email">Email:</label>
+                    <input className="input" type={email} name="email" defaultValue={email} onBlur={handleChange}></input>
+                </div>
+                <div className="field">
+                    <label className="label" htmlFor="message">Message:</label>
+                    <input className="textarea" name="message" rows="5" defaultValue={message} onBlur={handleChange}></input>
+                </div>
+                {errorMessage && (
+                    <div>
+                        <p className="is-danger">{errorMessage}</p>
+                    </div>
+                )}
+                <button className="button" data-testid="button" type="submit">Submit</button>
+            </form>
+        </div>
+    );
+}
+
+export default Contact;
